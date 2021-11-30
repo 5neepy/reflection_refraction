a8d5ea099cdf3771a3e63e36c24c081b4e1d02aa
unknown <martilambov@gmail.com>
2021-11-30 20:14:53 +0200

    Fix typing aliases and remove some inaccuracies

--- reflection_refraction.py
+++ reflection_refraction.py
@@ -3,9 +3,9 @@ import math
 from matplotlib import pyplot as plt
 import typing
 
-distance = 5
+DISTANCE = 5
 
-def intro():
+def intro() -> None:
     '''Introduction to the project.
     '''
     
@@ -13,7 +13,7 @@ def intro():
     print("By Martin Lambov")
     print("GitHub: https://github.com/5neepy")
 
-def check_alfa(alfa: float):
+def check_alfa(alfa: float) -> None:
     '''Check if the equation is possible by giving alfa certain range.
 
     :param alfa: Angle of incidence.
@@ -23,7 +23,7 @@ def check_alfa(alfa: float):
         print("\nAlfa can't be equal or more than 90 and can't be equal or less than 0.\n")
         exit()
 
-def check_index_of_refr(n1: float, n2: float):
+def check_index_of_refr(n1: float, n2: float) -> None:
     '''Check if the refraction indices are equal.
     
     :param n1: Index of refraction of the first medium
@@ -34,7 +34,7 @@ def check_index_of_refr(n1: float, n2: float):
         print("\nIf n_1 = n_2 we don't have Reflection/Refraction of light.\n")
         exit()
 
-def get_vars() -> float:
+def get_vars() -> tuple[float,float,float]:
     '''Getting the variables from the user.
     
     :return: Angle of incidence.
@@ -54,7 +54,7 @@ def get_vars() -> float:
 
     return alfa, n1, n2
 
-def calculate_refl_angl(alfa: float, n1: float, n2: float) -> float:
+def calculate_refr_angl(alfa: float, n1: float, n2: float) -> tuple[float,float,float]:
     '''Calculate the refraction angle.
     
     :param alfa: Angle of incidence
@@ -79,10 +79,10 @@ def calculate_refl_angl(alfa: float, n1: float, n2: float) -> float:
 
     return alfa, alfa_prim, beta
  
-def calc_ref_plot(alfa: float, beta:float) -> float:
+def calc_ref_plot(alfa: float, beta:float) -> tuple[float,float,float]:
     '''Calculate the positions of the points in the plot.
     :param alfa: Angle of incidence
-    :return: beta: Angle of refraction
+    :param: beta: Angle of refraction
     :return: Start cordinates of the incident beam
     :return: Start cordinates of the reflected beam
     :return: Start cordinates of the refracted beam
@@ -90,8 +90,8 @@ def calc_ref_plot(alfa: float, beta:float) -> float:
     
     # Incident beam = a:
     alfa_2 = 90+alfa
-    X_1 = (0+distance*math.cos(math.radians(alfa_2)))
-    Y_1 = (0+distance*math.sin(math.radians(alfa_2)))
+    X_1 = (0+DISTANCE*math.cos(math.radians(alfa_2)))
+    Y_1 = (0+DISTANCE*math.sin(math.radians(alfa_2)))
 
     # Reflected beam = a_1:
     X_2 = -X_1
@@ -100,8 +100,8 @@ def calc_ref_plot(alfa: float, beta:float) -> float:
     if beta is not None:
         # Refracted beam = b:
         beta_2 = 270+beta
-        X_3 = (0+distance*math.cos(math.radians(beta_2)))
-        Y_3 = (0+distance*math.sin(math.radians(beta_2)))
+        X_3 = (0+DISTANCE*math.cos(math.radians(beta_2)))
+        Y_3 = (0+DISTANCE*math.sin(math.radians(beta_2)))
         p3 = [X_3, Y_3]
     else:
         p3 = None
@@ -133,8 +133,8 @@ def matplotlib_graph(p1: float, p2: float, p3: float, n1: float, n2: float) -> s
     ax.spines['right'].set_color('none')
 
     # Set Cord. sys. Range and making 1x=1y
-    ax.set_xlim([-distance, distance])
-    ax.set_ylim([-distance, distance])
+    ax.set_xlim([-DISTANCE, DISTANCE])
+    ax.set_ylim([-DISTANCE, DISTANCE])
     plt.axis('equal')
 
     # Hide numbers
@@ -143,16 +143,16 @@ def matplotlib_graph(p1: float, p2: float, p3: float, n1: float, n2: float) -> s
     
     # Incident beam
     dc1 = [p1[0], p1[1]]
-    plt.arrow(x=distance*p1[0], y=distance*p1[1], dx=distance*-dc1[0], dy=distance*-dc1[1], facecolor=COLOR_OF_INCIDENT_BEAM, width=0.4, head_width=1, head_length=1.5, length_includes_head=True)
+    plt.arrow(x=DISTANCE*p1[0], y=DISTANCE*p1[1], dx=DISTANCE*-dc1[0], dy=DISTANCE*-dc1[1], facecolor=COLOR_OF_INCIDENT_BEAM, width=0.4, head_width=1, head_length=1.5, length_includes_head=True)
 
     # Reflected beam
     c2, dc2 = [0, 0], [p2[0], p2[1]]
-    plt.arrow(x=c2[0], y=c2[1], dx=distance*dc2[0], dy=distance*dc2[1], facecolor=COLOR_OF_REFLECTED_AND_REFRACTED_BEAM, width=0.4, head_width=1, head_length=1.5, length_includes_head=True)
+    plt.arrow(x=c2[0], y=c2[1], dx=DISTANCE*dc2[0], dy=DISTANCE*dc2[1], facecolor=COLOR_OF_REFLECTED_AND_REFRACTED_BEAM, width=0.4, head_width=1, head_length=1.5, length_includes_head=True)
 
     if p3 is not None:
         # Refracted beam
         c3, dc3 = [0, 0], [p3[0], p3[1]]
-        plt.arrow(x=c3[0], y=c3[1], dx=distance*dc3[0], dy=distance*dc3[1] , facecolor=COLOR_OF_REFLECTED_AND_REFRACTED_BEAM, width=0.4, head_width=1, head_length=1.5, length_includes_head=True)
+        plt.arrow(x=c3[0], y=c3[1], dx=DISTANCE*dc3[0], dy=DISTANCE*dc3[1] , facecolor=COLOR_OF_REFLECTED_AND_REFRACTED_BEAM, width=0.4, head_width=1, head_length=1.5, length_includes_head=True)
 
     # Legend
     plt.legend(["Incident beam", "Reflected beam", "Refracted beam"], loc='lower left')
@@ -164,10 +164,10 @@ def matplotlib_graph(p1: float, p2: float, p3: float, n1: float, n2: float) -> s
 
     return plt.show()
 
-def main():
+def main() -> None:
     intro()
     alfa, n1, n2 = get_vars()
-    alfa, alfa_prim, beta = calculate_refl_angl(alfa, n1, n2)
+    alfa, alfa_prim, beta = calculate_refr_angl(alfa, n1, n2)
     p1, p2, p3 = calc_ref_plot(alfa, beta)
     
     print("\nalfa = " + str(alfa))
@@ -180,7 +180,5 @@ def main():
         exit()
 
     matplotlib_graph(p1, p2, p3, n1, n2)
-
-    # print("\nP1 = ( " + str(p1) + " )" "\nP2 = ( " + str(p2) + " )\nP3 = ( " + str(p3) + " )\n")
 
 main()
